50a5b73f4c902b650e56f5137cff373a67fd035f
Anthony Ikwuegbu <ikwuegbuanthony02@gmail.com>
2022-01-09 16:08:48 +0100

    Added updates

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -13,19 +13,15 @@ const Skills = () => {
             <p><img src="https://img.icons8.com/color/40/000000/nodejs.png" alt="node-logo"/>Node.js</p>
         </div>
         <div>
-            <strong>Libraries:</strong>
+            <strong>Libraries/Frameworks:</strong>
             <p><img src="https://img.icons8.com/color/35/000000/bootstrap.png" alt="boot-logo"/>Bootstrap</p>
             <p><img src="https://img.icons8.com/color/35/000000/bkm-express.png" alt="express-logo"/>Express.js</p>
             <p><img src="https://img.icons8.com/color/35/000000/material-ui.png" alt="mat-ui-logo"/>Material-ui</p>
             <p><img src="https://img.icons8.com/officel/35/000000/react.png" alt="react-logo"/>React js</p>
+            <p><img src="https://img.icons8.com/color/48/000000/wind.png" alt="tailwind-logo"/>TailwindCSS</p>
+            <p><img src="https://img.icons8.com/ios/50/000000/circled-n.png" alt="next-logo"/>Nextjs</p>
         </div>
-        <div>
-            <strong>Tools:</strong>
-            <p><img src="https://img.icons8.com/color/35/000000/github--v1.png" alt="git-logo"/>Git & GitHub</p>
-            <p><img src="https://img.icons8.com/fluent/35/000000/chrome.png" alt="chrome-logo"/>Chrome Dev</p>
-            <p><img src="https://img.icons8.com/plasticine/35/000000/m.png" alt="mongoose-logo"/>Mongoose</p>
-            <p><img src="https://img.icons8.com/office/35/000000/code.png" alt="vscode-logo"/>VsCode</p>
-        </div>
+      
         <div>
             <strong>Databases:</strong>
             <p><img src="https://img.icons8.com/dotty/35/000000/sql.png" alt="sql-logo"/>SQL</p>
