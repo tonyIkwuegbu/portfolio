1aabc981e567155e70b94b3d4cad3eb8a6e98d71
Anthony Ikwuegbu <ikwuegbuanthony02@gmail.com>
2022-01-09 16:07:34 +0100

    Updated home

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,30 +1,24 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 import "./style.css";
 
 const Home = () => {
-    return (
-        <div className= "home">
-            <div className= "title">
-            <h1>
-                <p className="name">Hi,</p>
-                <p className="name">I'm Tony</p>
-                <p className="job">Software Developer</p>
-            </h1>
-            <Link to="about">
-            <button>More Info</button>
-
-            </Link>
-
-            </div>
-            <div className= "person">
-                <img
-                src={`${process.env.PUBLIC_URL}/main.png`}
-                alt = "person"
-                />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="home">
+      <div className="title">
+        <h1>
+          <p className="name">Hi,</p>
+          <p className="name">I'm Tony</p>
+          <p className="job">Front-end Developer</p>
+        </h1>
+        <Link to="about">
+          <button>More Info</button>
+        </Link>
+      </div>
+      <div className="person">
+        <img src={`${process.env.PUBLIC_URL}/main.png`} alt="person" />
+      </div>
+    </div>
+  );
+};
 
 export default Home;
-
