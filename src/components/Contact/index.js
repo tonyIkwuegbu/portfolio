e802059e1cecd9fd18d545381deff361f630e9d7
Anthony Ikwuegbu <ikwuegbuanthony02@gmail.com>
2022-04-16 13:42:24 +0100

    updated portfolio

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,112 +1,116 @@
 import React, { useState } from "react";
-import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
 import TextField from "@mui/material/TextField";
 import "./style.css";
 import Button from "@mui/material/Button";
 import emailjs from "emailjs-com";
 
 const Contact = () => {
-  const [message, setMessage] = useState(
-    <p>Please enter your details correctly</p>
-  );
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [deliver, setDeliver] = useState("");
 
-  function sendEmail(e) {
+  const sendEmail = (e) => {
     e.preventDefault();
 
     emailjs
-      .sendForm(
-        "gmail",
-        "template_5ztfryj",
-        e.target,
-        "user_286yNR8cdPOFzEECcKLAB"
+      .send(
+        "service_jsvdjc8",
+        "template_bwuzqjc",
+        { name, email, message },
+        "jG0jfyteyiFBUYSrM"
       )
-      .then(
-        () => {
-          setMessage("Thank you  for your submission!");
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-    e.target.reset();
-  }
+      .then((response) => {
+        console.log("SUCCESS", response.status, response.text);
+        setEmail("");
+        setName("");
+        setMessage("");
+        setTimeout(() => {
+          setDeliver(`Thank you ${name}, I'll get back to you soon.`);
+        }, 5000);
+      })
+      .catch((error) => {
+        console.log("oops! Failed", error);
+      });
+  };
 
   return (
     <div className="contact">
-      <h1>Contact me!</h1>
+      <div>
+        <h1>Contact me!</h1>
 
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d252166.39753871926!2d7.254267964001987!3d9.054646210491764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104e745f4cd62fd9%3A0x53bd17b4a20ea12b!2sAbuja!5e0!3m2!1sen!2sng!4v1621591763826!5m2!1sen!2sng"
-        title="abujaMap"
-        allowFullScreen=""
-        aria-hidden="false"
-        tabIndex="0"
-      ></iframe>
-      <h4>Address</h4>
-      <p>Federal Capital Territory, Abuja</p>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d252166.39753871926!2d7.254267964001987!3d9.054646210491764!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104e745f4cd62fd9%3A0x53bd17b4a20ea12b!2sAbuja!5e0!3m2!1sen!2sng!4v1621591763826!5m2!1sen!2sng"
+          title="abujaMap"
+          allowFullScreen=""
+          aria-hidden="false"
+          tabIndex="0"
+        ></iframe>
+        <h4>Address: Federal Capital Territory, Abuja</h4>
+      </div>
 
-      <h4> Contact me directly on Whatsapp</h4>
-      <a href="https://wa.link/enmows" target="_blank" rel="noreferrer">
-        <img
-          src="https://img.icons8.com/color/452/whatsapp--v1.png"
-          alt="whatsapp-icon"
-        />
-      </a>
-      <br />
-      <br />
+      <div>
+        <h4> Contact me directly on Whatsapp</h4>
+        <a href="https://wa.link/enmows" target="_blank" rel="noreferrer">
+          <img
+            src="https://img.icons8.com/color/452/whatsapp--v1.png"
+            alt="whatsapp-icon"
+          />
+        </a>
+      </div>
 
-      <h4>
-        Interested in working together?please fill out the form below and i'll
-        get back to you as soon as i can.
-      </h4>
-      <form onSubmit={sendEmail}>
-        <Box
-          component="form"
-          sx={{
-            "& > :not(style)": { m: 1, width: "35ch" },
-          }}
-          noValidate
-          autoComplete="off"
-        >
+      <Paper sx={{ mt: 2, width: "35ch", p: 3, backgroundColor: "#EEEEEE" }}>
+        <h4>
+          Interested in working together? please fill out the form below...
+        </h4>
+        <h4 style={{ color: "green" }}>{deliver}</h4>
+        <form onSubmit={sendEmail}>
           <TextField
-            id="filled-basic"
+            sx={{ mb: 2, mt: 2, width: "35ch" }}
             name="name"
             type="text"
-            label="Your Name"
-            variant="filled"
+            label="Name"
+            variant="outlined"
             required
+            placeholder="please enter your name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
           />
-          <br />
           <TextField
-            id="filled-basic"
+            sx={{ mb: 2, width: "35ch" }}
             name="email"
-            label="Your Email"
+            label="Email"
             type="email"
-            variant="filled"
+            placeholder="please enter your email"
+            variant="outlined"
             required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
-          <br />
           <TextField
-            id="filled-basic"
+            sx={{ mb: 2, width: "35ch" }}
             name="message"
             label="Your message"
             placeholder="Tell me a little about your project..."
             multiline
-            variant="filled"
+            variant="outlined"
             required
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
           />{" "}
-          <br />
           <div>
-            <Button variant="contained" color="primary" type="submit">
+            <Button
+              variant="contained"
+              color="primary"
+              type="submit"
+              sx={{ mb: 2, width: "25ch" }}
+            >
               Send
             </Button>
-            <Button variant="contained" color="secondary" type="reset">
-              Clear
-            </Button>
           </div>
-          <p> {message} </p>
-        </Box>
-      </form>
+        </form>
+      </Paper>
     </div>
   );
 };
